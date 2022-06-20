c18b31e1b6d315f63bd3dd9e94b986e5cef0aa85
khoacao-ccdk <61849146+khoacao-ccdk@users.noreply.github.com>
2022-06-20 09:24:16 -0500

    Update ListViewVehicleRepair.kt

--- RepairShop/app/src/main/java/com/codycao/repairshop/database/ListViewVehicleRepair.kt
+++ RepairShop/app/src/main/java/com/codycao/repairshop/database/ListViewVehicleRepair.kt
@@ -33,7 +33,7 @@ class ListViewVehicleRepair() : BaseAdapter() {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         var view = convertView
         if(view == null){
-            view = LayoutInflater.from(context).inflate(R.layout.listview_results_row, null)
+            view = LayoutInflater.from(context).inflate(R.layout.listview_results_row, parent, false)
         }
         val vehicleRepair = vehicleRepairList[position]
         val text_year_make_model = view!!.findViewById<TextView>(R.id.text_year_make_model)
